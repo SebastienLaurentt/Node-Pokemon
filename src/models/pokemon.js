e68f873efc68a1bb2e4da2efcf8184ed68a312e0
SebastienLaurentt <slaurent.26@gmail.com>
2023-10-06 18:54:38 +0200

    create validors with sequelize in pokemon.js AND recatch error status from 500 to 400 in create and update routes files

--- src/models/pokemon.js
+++ src/models/pokemon.js
@@ -1,4 +1,7 @@
-/* L’API Rest et la Base de données : Créer un modèle Sequelize */
+
+const validTypes = ['Plante', 'Poison', 'Feu', 'Eau', 'Insecte', 'Vol', 'Normal', 'Electrik', 'Fée']
+
+
 module.exports = (sequelize, DataTypes) => {
     return sequelize.define('Pokemon', {
       id: {
@@ -8,19 +11,54 @@ module.exports = (sequelize, DataTypes) => {
       },
       name: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        unique: {
+          msg: 'Le nom est déja pris.'
+        },
+        validate: {
+          notEmpty: { msg: "Le nom ne peut être vide"},
+          notNull : { msg : 'Le nom est une propriété requise.'}
+        }
       },
       hp: {
         type: DataTypes.INTEGER,
-        allowNull: false
+        allowNull: false,
+        validate: {
+          isInt: { msg: "Utilisez uniquement des nombres entiers pour les points de vie."},
+          min: {
+            args: [0],
+            msg : 'Les points de vie doivent être supérieurs ou égales à 0.'
+          },
+          max: {
+            args: [999],
+            msg : 'Les points de vie doivent être inférieurs ou  égales à 999.'
+          },
+          notNull : { msg : 'Les points de vie sont une propriété requise.'}
+        }
       },
       cp: {
         type: DataTypes.INTEGER,
-        allowNull: false
+        allowNull: false,
+        validate: {
+          isInt: { msg: "Utilisez uniquement des nombres entiers pour les points de dégâts."},
+          min: {
+            args: [0],
+            msg : 'Les points de dégats doivent être supérieurs ou égales à 0.'
+          },
+          max: {
+            args: [99],
+            msg : 'Les points de dégats doivent être inférieurs ou  égales à 99.'
+          },
+          notNull : { msg : 'Les points de dégâts sont une propriété requise.'}
+        }
       },
       picture: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        validate: {
+          isUrl: { msg: "Utilisez uniquement une URL valide pour l\'image."},
+          notNull : { msg : 'L\'image est une propriété requise.'}
+        }
       },
       types: {
         type: DataTypes.STRING,
@@ -30,6 +68,21 @@ module.exports = (sequelize, DataTypes) => {
         },
         set(types){
           this.setDataValue('types', types.join())
+        },
+        validate: {
+          isTypesValid(value) {
+            if(!value) {
+              throw new Error ('Un pokémon doit avoir au moins un type.')
+            }
+            if(value.split(',').length > 3) {
+              throw new Error('Un pokémon ne peux pas avoir plus de trois types.')
+            }
+            value.split(',').forEach(type => {
+              if(!validTypes.includes(type)) {
+                throw new Error(`Le type d'un pokémon doit appartenir à la liste suivante : ${validTypes}`)
+              }
+            });
+          }
         }
       }
     }, {
--- src/routes/updatePokemon.js
+++ src/routes/updatePokemon.js
@@ -1,3 +1,4 @@
+const { ValidationError } = require('sequelize')
 const { Pokemon } = require('../db/sequelize')
   
 module.exports = (app) => {
@@ -10,13 +11,19 @@ module.exports = (app) => {
       return Pokemon.findByPk(id).then(pokemon => {
         if(pokemon === null) {
             const message = 'Le pokémon demandé n\existe pas. Réessayer avec un autre identifiant';
-            return res.statut(404).json({message})
+            return res.status(404).json({message})
         }
         const message = `Le pokémon ${pokemon.name} a bien été modifié.`
         res.json({message, data: pokemon })
       })
     })
     .catch(error => {
+        if(error instanceof ValidationError) {
+          return res.status(400).json({ message: error.message, data:error})
+        }
+        if(error instanceof UniqueConstraintError) {
+          return res.status(400).json( {message: error.message, data:error})
+        }
         const message = `La liste des pokémons n'a pas pu être récupérée. Réessayer dans quelques instants.`
         res.status(500).json({ message, data:error})
       })
