1ee9cbd8f26b57a5b9807cf4ff39495e77bac76a
SebastienLaurentt <slaurent.26@gmail.com>
2023-10-07 15:33:47 +0200

    add more precise request on finAllPokemons

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -1,8 +1,32 @@
 const { Pokemon } = require('../db/sequelize')
+const { Op } = require('sequelize')
   
 module.exports = (app) => {
   app.get('/api/pokemons', (req, res) => {
-    Pokemon.findAll()
+    if(req.query.name) {
+      const name = req.query.name
+      const limit = parseInt(req.query.limit) || 5
+
+      if(name.length <2) {
+        const message = 'Le terme de recherche doit contenir au moins 2 caractères'
+        return res.status(400).json( {message})
+      }
+
+      return Pokemon.findAndCountAll( {
+        where: {
+          name: { // Ici name est la propriété du modèle pokémon
+            [Op.like]: `%${name}%` // 'name' est le critère de la recherche
+            }
+          },
+          order: ['name'],
+          limit: 5
+      })
+      .then( ({count, rows}) => {
+        const message = `Il y a ${count} pokémons qui correspondent à votre recherche ${name}.`
+        res.json({ message, data: rows})
+      })
+    } else {
+      Pokemon.findAll( {order: ['name']} ) 
       .then(pokemons => {
         const message = 'La liste des pokémons a bien été récupérée.'
         res.json({ message, data: pokemons })
@@ -11,5 +35,7 @@ module.exports = (app) => {
         const message = `La liste des pokémons n'a pas pu être récupérée. Réessayer dans quelques instants.`
         res.status(500).json({ message, data:error})
       })
+    }
+
   })
 }
