4e4e2ffee77e627cd26b5216e17cb321b285e5e7
SebastienLaurentt <slaurent.26@gmail.com>
2023-10-07 15:58:14 +0200

    add user model

---
+++ src/models/user.js
@@ -0,0 +1,16 @@
+/* Authentification : Créer un modèle User avec Sequelize */
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('User', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      username: {
+        type: DataTypes.STRING
+      },
+      password: {
+        type: DataTypes.STRING
+      }
+    })
+  }
